8ee52e4cb1218424830a338908dd7d1537221f25
Mike Santos <mike.santos@contraprova.com.br>
2024-06-21 13:09:00 -0300

    chore: add eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}).then(() => console.log('HTTP Server Running!'))
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => console.log('HTTP Server Running!'))
